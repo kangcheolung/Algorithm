e57cf5726b02280c7b1a6129b0e7ff478a485bb7
kangcheolung <skhu202114111@gmail.com>
2025-12-15 16:31:38 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 144 ms, Memory: 15736 KB -BaekjoonHub

---
+++ 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int[] numbers = new int[5];
+		for(int i=0; i<5; i++){
+			numbers[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int answer = 1;
+		
+		while(true){
+			int count = 0;
+			for(int i=0; i<5; i++){
+				if(answer % numbers[i] == 0){
+					count++;
+				}
+			}
+			
+			if (count >= 3){
+				break;
+			}
+			answer++;
+		}
+		
+		System.out.println(answer);
+
+		
+	}
+}
