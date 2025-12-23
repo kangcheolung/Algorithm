9b6f597e016d9a7dbd325727bd4a58ff069d7f1d
kangcheolung <skhu202114111@gmail.com>
2025-12-23 20:55:37 +0900

    [Silver V] Title: 영화감독 숌, Time: 276 ms, Memory: 86544 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int count = 0;
+        int number = 666;
+
+        while (true){
+            if (String.valueOf(number).contains("666")){
+                count++;
+
+                if (count == N){
+                    System.out.println(number);
+                    break;
+                }
+            }
+            number++;
+        }
+
+    }
+}
