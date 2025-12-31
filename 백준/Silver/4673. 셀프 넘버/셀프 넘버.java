38e04330680486315b4e0e0708b0a1efde6ad191
kangcheolung <skhu202114111@gmail.com>
2025-12-31 16:23:41 +0900

    [Silver V] Title: 셀프 넘버, Time: 124 ms, Memory: 14600 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -0,0 +1,27 @@
+public class Main {
+	public static void main(String[] args) {
+		boolean[] hasGenerator = new boolean[10001];
+
+		for (int i = 1; i <= 10000; i++) {
+			int dn = d(i);
+			if (dn <= 10000) {
+				hasGenerator[dn] = true;
+			}
+		}
+
+		for (int i = 1; i <= 10000; i++) {
+			if (!hasGenerator[i]) {
+				System.out.println(i);
+			}
+		}
+	}
+
+	static int d(int n) {
+		int sum = n;
+		while (n > 0) {
+			sum += n % 10;
+			n /= 10;
+		}
+		return sum;
+	}
+}
