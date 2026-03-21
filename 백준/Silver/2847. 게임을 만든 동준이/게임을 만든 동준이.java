dd41d9a615b26d58a8e1a0a43c26e0bde7a5ab15
kangcheolung <skhu202114111@gmail.com>
2026-03-21 13:33:08 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n =  Integer.parseInt(br.readLine()); 
+
+		int[] arr = new int[n];
+
+		for(int i=0; i<n; i++){
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		int answer = 0;
+
+		for(int i=n-2; i>=0; i--){
+			if(arr[i] >= arr[i+1]){
+				int reduce = arr[i] - (arr[i+1] -1);
+				answer = answer + reduce;
+				arr[i] = arr[i+1] -1;
+			}
+		}
+		System.out.println(answer);
+	}
+}
